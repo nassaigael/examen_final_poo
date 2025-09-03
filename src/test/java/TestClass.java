3e942634824da6c2b62ac69478dd2993bb010047
nassaigael <hei.nassai@gmail.com>
2025-09-03 16:51:55 +0300

    resolve conflit

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class TestClass {
+    @Test
+    void addTest(){
+        assertTrue(55+5 == 57+3);
+    }
+}
