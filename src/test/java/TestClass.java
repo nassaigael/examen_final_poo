bf9bc227b7abccac24a3ccd7ad3d44c966d03217
nassaigael <hei.nassai@gmail.com>
2025-09-04 08:44:05 +0300

    add test class

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class TestClass {
+    @Test
+    void addTest(){
+        assertTrue(Math.pow(5, 2) == 25);
+    }
+}
